43b565ecc25a4f066f011757a662452eb68dbb57
Joofes <64694259+Joofes@users.noreply.github.com>
2023-11-23 10:32:51 -0500

    camfix

---
+++ Alchemy Project/Assets/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerTransform;  // Reference to the player's transform
+    public float smoothness = 0.5f;     // How smoothly the camera follows the player
+
+    private void Update()
+    {
+        if (playerTransform != null)
+        {
+            // Calculate the new position for the camera using Lerp
+            Vector3 targetPosition = new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothness * Time.deltaTime);
+        }
+    }
+}
