983ad57f3eb8133f3d12cd1557de2d99762aa5e2
Neb465 <benito.f.nie@gmail.com>
2023-11-23 18:44:20 -0500

    Hotbar selection

---
+++ Alchemy Project/Assets/Scripts/HotbarController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HotbarController : MonoBehaviour
+{
+    public int HotbarSlotSize => gameObject.transform.childCount;
+    private List<Image> hotbarSlots = new List<Image>();
+
+    KeyCode[] hotbarKeys = new KeyCode[] {KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3 };
+
+    private void Start()
+    {
+        SetUpHotbarSlots();
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < hotbarKeys.Length; i++)
+        {
+            if (Input.GetKeyDown(hotbarKeys[i]))
+            {
+                Debug.Log("Use item: " + i);
+                return;
+            }
+        }
+    }
+
+    private void SetUpHotbarSlots()
+    {
+        for(int i = 0; i < HotbarSlotSize; i++)
+        {
+            Image slot = gameObject.GetComponentInChildren<Image>();
+            hotbarSlots.Add(slot);
+        }
+    }
+}
