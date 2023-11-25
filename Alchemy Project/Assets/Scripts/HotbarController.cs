cc6af34763ccfe13fe687f6048820757b76ad8c1
Neb465 <benito.f.nie@gmail.com>
2023-11-25 11:57:51 -0500

    Finished hotbar selection

--- Alchemy Project/Assets/Scripts/HotbarController.cs
+++ Alchemy Project/Assets/Scripts/HotbarController.cs
@@ -7,8 +7,9 @@ public class HotbarController : MonoBehaviour
 {
     public int HotbarSlotSize => gameObject.transform.childCount;
     private List<Image> hotbarSlots = new List<Image>();
+    private int currentIndex = 0;
 
-    KeyCode[] hotbarKeys = new KeyCode[] {KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3 };
+    KeyCode[] hotbarKeys = new KeyCode[] {KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3};
 
     private void Start()
     {
@@ -17,14 +18,33 @@ public class HotbarController : MonoBehaviour
 
     private void Update()
     {
-        for (int i = 0; i < hotbarKeys.Length; i++)
+        checkForKey();
+        return;
+    }
+
+    private void checkForKey()
+    {
+
+        if (Input.GetKeyDown(hotbarKeys[0]))
         {
-            if (Input.GetKeyDown(hotbarKeys[i]))
-            {
-                hotbarSlots[i].transform.GetChild(i).GetComponent<Image>().color = Color.yellow;
-                return;
-            }
+            hotbarSlots[0].transform.GetChild(0).GetComponent<Image>().color = Color.yellow;
+            hotbarSlots[1].transform.GetChild(1).GetComponent<Image>().color = Color.green;
+            hotbarSlots[2].transform.GetChild(2).GetComponent<Image>().color = Color.green;
         }
+        if (Input.GetKeyDown(hotbarKeys[1]))
+        {
+            hotbarSlots[1].transform.GetChild(1).GetComponent<Image>().color = Color.yellow;
+
+            hotbarSlots[0].transform.GetChild(0).GetComponent<Image>().color = Color.green;
+            hotbarSlots[2].transform.GetChild(2).GetComponent<Image>().color = Color.green;
+        }
+        if (Input.GetKeyDown(hotbarKeys[2]))
+        {
+            hotbarSlots[2].transform.GetChild(2).GetComponent<Image>().color = Color.yellow;
+            hotbarSlots[1].transform.GetChild(1).GetComponent<Image>().color = Color.green;
+            hotbarSlots[0].transform.GetChild(0).GetComponent<Image>().color = Color.green;
+        }
+        
     }
 
     private void SetUpHotbarSlots()
